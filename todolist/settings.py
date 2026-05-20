2cd04bfebf15f2fcd54b913c9bbe40f76e50c113
Lucas <lcsg2103@gmail.com>
2026-05-20 15:46:56 -0300

    banco de dados alterado de sqlite3 para postgresql

--- todolist/settings.py
+++ todolist/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_db',
+        'USER': 'root',
+        'PASSWORD': 'S&rver@2220#250',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
